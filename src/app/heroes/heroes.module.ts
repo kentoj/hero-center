04e6e83a1db142ed293df00eef0b471084d260fc
Kent O. Johnson <kentoj@gmail.com>
2016-11-27 14:47:55 -0700

    added heroes module

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeroesComponent } from './heroes.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [HeroesComponent]
+})
+export class HeroesModule { }
